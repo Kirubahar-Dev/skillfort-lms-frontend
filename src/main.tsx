2886a43bec81a2d70f5388614fcb324143c91ac6
SkillFort Deploy <deploy@skillfort.local>
2026-04-08 19:47:05 +0530

    Add AuthProvider to main.tsx to enable auth context
    - Wrap App with AuthProvider in React root
    - Enables useAuth() hook in LoginModule and other components
    - Fixes undefined context error in protected routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import { AuthProvider } from "./lib/auth";
 import { App } from "./App";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
