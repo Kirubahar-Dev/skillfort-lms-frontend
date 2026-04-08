c66ca7fdb318d5dae6833b0fe444d828af6643b5
SkillFort Deploy <deploy@skillfort.local>
2026-04-09 00:24:27 +0530

    Update canvas to use 100vh height matching reference implementation

--- src/components/effects/MouseFluidEffect.tsx
+++ src/components/effects/MouseFluidEffect.tsx
@@ -477,7 +477,7 @@ export function MouseFluidEffect() {
         top: 0,
         left: 0,
         width: '100%',
-        height: '100%',
+        height: '100vh',
         zIndex: -1,
         pointerEvents: 'none',
         display: 'block'
